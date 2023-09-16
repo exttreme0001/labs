6eab7a8e5162ec0865a6ceba1c7450b7c6305201
exttreme0001 <fpm.LukashovES@bsu.by>
2023-09-16 14:14:44 +0300

    Update pr1.cpp

--- pr1.cpp
+++ pr1.cpp
@@ -6,26 +6,22 @@ using namespace std;
 int main  () {
    double x, answ , a , b , func;
    int k , n ( 1 );
+    std:: cout << "Vvedite x "<< endl ;
     std:: cin >> x;
+    std:: cout << "Vvedite tochnost  "<< endl ;
     std:: cin >> k;
 
   func = log (1+x );
     std:: cout << setprecision(k) << func << endl ;
 
-    if ( k < 1 || x>1 || x <= -1 ){std:: cout <<"used wrong x or k";return 0;}
-    else
-    {
-
-    /*func > 0 && func < pow(10.0,-k)*/
-  while ( a != b )  {
-        answ = answ + pow (x ,n ) * pow (-1, n - 1) / n ; n++;
-         a = int ( answ * pow (10,k)), b = int ( func * pow (10,k ) ); // пока все k цифры не совпадут не заканчивать
-        std:: cout << answ << endl << a << endl << b << endl;
+    if ( k < 1 || x>1 || x <= -1|| k>11 ){std:: cout <<"used wrong x or k";return 0;}
+    else {
+        while ( a != b )  {
+              answ = answ + pow (x ,n ) * pow (-1, n - 1) / n ; n++;
+              a = int ( answ * pow (10,k)), b = int ( func * pow (10,k ) ); // пока все k цифры не совпадут не заканчивать
+            //  std:: cout << answ << endl << a << endl << b << endl; // выводит значение на каждом к, не обязательно
     }
-        std:: cout <<setprecision(k)<<answ<<endl;
-
-
-
+        std:: cout << setprecision(k+1) << answ << endl;
     }
 
     return 0;
